1ac6b35a14cc02fadc7d8f2e1ff23d5c2b09e946
robert boudreaux <boudreaux.r@gmail.com>
2023-08-03 22:17:50 +0200

    submitloopexercise

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(namesArray, surprise) {
+  const cards = [];
+  for (let i = 0; i < namesArray.length; i++) {
+    let namesArray = ["Guadalupe", "Ollie", "Aki"];
+    const message = `Thank you, ${namesArray[i]}, for the wonderful ${surprise} gift!`;
+    cards.push(message);
+    
+  }
+  return cards;
+}
+function countDown() {
+  for (let i = number; i >= 0; i--) {
+    console.log(i);
+  }
+}
+
+
