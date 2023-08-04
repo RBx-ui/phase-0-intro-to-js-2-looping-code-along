d274fd237e34eef912db58d13fc80f9998be831f
robert boudreaux <boudreaux.r@gmail.com>
2023-08-04 11:49:34 +0200

    submitloopingcodealong

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 // Code your solutions in this file
 
 function writeCards(namesArray, surprise) {
-  const cards = [];
+  let cards = [];
   for (let i = 0; i < namesArray.length; i++) {
     let namesArray = ["Guadalupe", "Ollie", "Aki"];
     const message = `Thank you, ${namesArray[i]}, for the wonderful ${surprise} gift!`;
     cards.push(message);
-    
   }
   return cards;
-
-function countDown(number) {
-  let number = 10;
-  while (number >= 0) {
-    console.log(number--);
-  }
 }
-
-
+function countDown(integer) {
+   let someNumber = 9;
+   while (someNumber >= 0) {
+     console.log(someNumber--);
+  }
+  return console.log()
+}
